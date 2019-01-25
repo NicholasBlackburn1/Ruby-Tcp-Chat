74bff8dc8c898fff4053ab5d54cb3174318fdde5
BuildTools <frc5740@gmail.com>
2019-01-25 09:02:49 -0500

    addes test

--- server.rb
+++ server.rb
@@ -5,6 +5,8 @@ server = TCPServer.open(3000) # Server would listen on port 3000
 loop{                         # Servers run forever
    client_connection = server.accept # Establish client connect connection
    client_connection.puts(Time.now) # Send the time to the client
+   client_connection.puts(Date.now)
+   client_connection.puts("TEst")
    client_connection.puts("Closing the connection with #{client_connection}")
    client_connection.close      # Disconnect from the client
 }
