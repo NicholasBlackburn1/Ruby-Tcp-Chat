4e65c1efe6d12b7b991e85f9163ea0193eac3b97
BuildTools <frc5740@gmail.com>
2019-01-25 09:01:28 -0500

    added new tcp socet server

--- server.rb
+++ server.rb
@@ -1,9 +1,10 @@
 require "socket"
-class Server
-  def initialize(port, ip)
-    @server = nil
-    @connections  = {}
-    @rooms = {}
-    @clients = {}
-  end
-end
+
+puts "Starting the Server..................."
+server = TCPServer.open(3000) # Server would listen on port 3000
+loop{                         # Servers run forever
+   client_connection = server.accept # Establish client connect connection
+   client_connection.puts(Time.now) # Send the time to the client
+   client_connection.puts("Closing the connection with #{client_connection}")
+   client_connection.close      # Disconnect from the client
+}
